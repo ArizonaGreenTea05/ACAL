dde1f3f0f03aed700929bc07a23bb2a4053229b7
Nimona <84347307+ArizonaGreenTea05@users.noreply.github.com>
2026-01-20 13:51:12 +0100

    Add configuration support for appsettings.Development.json & environment variables (#25)
    * Add support for appsettings.Development.json
    
    * Add support for environment variables
    
    * Cleanup

--- CalendarView/CalendarView.Shared/Utils/Initialization.cs
+++ CalendarView/CalendarView.Shared/Utils/Initialization.cs
@@ -21,10 +21,19 @@ namespace CalendarView.Shared.Utils
         {
             var appsettingsPaths = new[] { "../config/appsettings.json", "appsettings.json" };
             var path = appsettingsPaths.FirstOrDefault(File.Exists) ?? throw new FileNotFoundException("appsettings not found");
-            var appsettingsStream = File.OpenRead(path);
+            using var appsettingsStream = File.OpenRead(path);
             var appsettingsBuilder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonStream(appsettingsStream);
+                .AddJsonStream(appsettingsStream)
+                .AddEnvironmentVariables("ACAL");
+
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")
+                                  ?? Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
+            if (string.Equals(environmentName, "Development", StringComparison.OrdinalIgnoreCase))
+            {
+                appsettingsBuilder.AddJsonFile("appsettings.Development.json", optional: true);
+            }
+
             var appsettings = appsettingsBuilder.Build();
             calendars = new Calendars();
             appsettings.GetSection(nameof(Calendars)).Bind(calendars);
